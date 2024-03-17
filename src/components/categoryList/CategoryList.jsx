24534a559e3e7ed94fb01c023048bd583c2118cf
AdrienHq <adrienhecq@gmail.com>
2024-03-17 16:26:08 +0100

    feat: categories UI

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,9 +1,39 @@
 import React from "react";
 import styles from "./categoryList.module.css"
+import Image from "next/image";
+import Link from "next/link";
 
 const CategoryList = () => {
     return (
-        <div className={styles.container}>CategoryList</div>
+        <div className={styles.container}>
+            <h1 className={styles.title}>Categories</h1>
+            <div className={styles.categories}>
+                <Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
+                    <Image src="/style.png" alt="" width={32} height={32} className={styles.image}/>
+                    Category 1
+                </Link>
+                <Link href="/blog?cat=style" className={`${styles.category} ${styles.food}`}>
+                    <Image src="/style.png" alt="" width={32} height={32} className={styles.image}/>
+                    Category 2
+                </Link>
+                <Link href="/blog?cat=style" className={`${styles.category} ${styles.fashion}`}>
+                    <Image src="/style.png" alt="" width={32} height={32} className={styles.image}/>
+                    Category 3
+                </Link>
+                <Link href="/blog?cat=style" className={`${styles.category} ${styles.travel}`}>
+                    <Image src="/style.png" alt="" width={32} height={32} className={styles.image}/>
+                    Category 4
+                </Link>
+                <Link href="/blog?cat=style" className={`${styles.category} ${styles.travel}`}>
+                    <Image src="/style.png" alt="" width={32} height={32} className={styles.image}/>
+                    Category 4
+                </Link>
+                <Link href="/blog?cat=style" className={`${styles.category} ${styles.travel}`}>
+                    <Image src="/style.png" alt="" width={32} height={32} className={styles.image}/>
+                    Category 4
+                </Link>
+            </div>
+        </div>
     )
 }
 
