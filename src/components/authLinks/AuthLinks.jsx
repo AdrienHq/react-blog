c362310516f6da8aa9fb47a0a3b19c3a96d730a9
AdrienHq <adrienhecq@gmail.com>
2024-03-17 15:16:37 +0100

    feat: burger menu for mobile display

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+"use client"
+
+import React, {useState} from "react";
 import styles from "./authLinks.module.css"
 import Link from "next/link";
 
 const AuthLinks = () => {
+    const [open, setOpen] = useState(false)
 
     const status = "notAuthenticated"
     return (
@@ -19,8 +22,32 @@ const AuthLinks = () => {
                     <span className={styles.link}>Logout</span>
                 </>
             )}
+            <div className={styles.burger} onClick={() => setOpen(!open)}>
+                <div className={styles.line}></div>
+                <div className={styles.line}></div>
+                <div className={styles.line}></div>
+            </div>
+            {open && (
+                <div className={styles.responsiveMenu}>
+                    <Link href="/">Homepage</Link>
+                    <Link href="/">Contact</Link>
+                    <Link href="/">About</Link>
+                    {status === "notAuthenticated" ? (
+                        <Link href="/login">
+                            Login
+                        </Link>
+                    ) : (
+                        <>
+                            <Link href="/write">
+                                Write
+                            </Link>
+                            <span className={styles.link}>Logout</span>
+                        </>
+                    )}
+                </div>
+            )}
         </>
-    )
-}
+    );
+};
 
 export default AuthLinks
