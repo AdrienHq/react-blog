20b7fc221f8b0862ac8e41cb919019f546616c25
AdrienHq <adrienhecq@gmail.com>
2024-03-17 11:01:33 +0100

    feat: navbar base

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,12 @@
 import styles from "./homepage.module.css";
 import Link from "next/link";
+import Navbar from "@/components/Navbar/Navbar";
 
 export default function Home() {
-  return <div>
-    <Link href="/">
-      <h1>Mon titre Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, mollitia?</h1>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. A dolorem eligendi ex minima mollitia praesentium qui, sapiente ullam? Animi consequuntur dolor dolorem fugiat iste maiores nam numquam officiis pariatur saepe, sequi tempore veritatis vitae. Ab culpa deserunt dolor, eius, ex exercitationem, fugiat illum magnam perferendis repellendus tempore veniam voluptatem. Ad, aperiam atque autem consequatur corporis cum delectus eius eum exercitationem explicabo, facilis ipsa itaque iure laboriosam, maxime molestiae molestias nisi officia pariatur perferendis quisquam quo quod quos rerum sed sint tempora totam unde voluptas voluptates? At consequatur explicabo minus modi perferendis, soluta velit! Accusamus animi commodi corporis deleniti distinctio dolore doloremque earum enim est expedita fuga fugiat illo ipsam iure mollitia nam natus necessitatibus nemo odio officia omnis perspiciatis porro provident quibusdam quod ratione recusandae reiciendis rem reprehenderit sapiente sed sequi suscipit ut, vel vero voluptatem voluptates! Amet autem doloremque nemo vel! Autem in modi nesciunt officia possimus veniam, voluptatem?
-    </Link>
-  </div>;
+  return (
+      <div>
+        <Navbar/>
+      </div>
+  )
   
 }
