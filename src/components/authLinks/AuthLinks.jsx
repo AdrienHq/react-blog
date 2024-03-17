2a3fe006e59976335d5aa7f64e19c61af5c0701f
AdrienHq <adrienhecq@gmail.com>
2024-03-17 13:12:26 +0100

    feat: base components

---
+++ src/components/authLinks/AuthLinks.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./authLinks.module.css"
+import Link from "next/link";
+
+const AuthLinks = () => {
+
+    const status = "notAuthenticated"
+    return (
+        <>
+            {status === "notAuthenticated" ? (
+                <Link href="/login" className={styles.link}>
+                    Login
+                </Link>
+            ) : (
+                <>
+                    <Link href="/write" className={styles.link}>
+                        Write
+                    </Link>
+                    <span className={styles.link}>Logout</span>
+                </>
+            )}
+        </>
+    )
+}
+
+export default AuthLinks
