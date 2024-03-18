adc6cd0e9c731e616ace8f4b4fdfa92ea501d9c0
AdrienHq <adrienhecq@gmail.com>
2024-03-18 16:50:29 +0100

    refactor: change the name of some components

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import styles from "./page.module.scss"
+import styles from "./blogPage.module.scss"
 import CardList from "@/components/cardList/CardList";
 import Menu from "@/components/menu/Menu";
 
-const Page = () => {
+const BlogPage = () => {
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Title Blog</h1>
@@ -15,4 +15,4 @@ const Page = () => {
     )
 }
 
-export default Page
+export default BlogPage
