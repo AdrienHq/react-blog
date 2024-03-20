953557243cdc9b4aacf82f1dec6b4241e5332f17
AdrienHq <adrienhecq@gmail.com>
2024-03-20 15:09:19 +0100

    fix: resolve error with the local storage

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -5,11 +5,11 @@ import {createContext, useEffect, useState} from "react";
 export const ThemeContext = createContext();
 
 const getFromLocalStorage = () => {
-    if (typeof Window !== undefined) {
+    if (typeof window !== 'undefined' && window.localStorage) {
         const value = localStorage.getItem("theme");
         return value || "light";
     }
-    return "light";
+    return "light"; // Default value if localStorage is not available
 }
 
 export const ThemeContextProvider = ({children}) => {
