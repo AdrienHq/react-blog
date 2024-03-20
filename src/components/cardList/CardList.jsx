438f554ae8c2528368e29d5a7b05b2975f6061c8
AdrienHq <adrienhecq@gmail.com>
2024-03-20 12:04:16 +0100

    feat: pagination for posts

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -21,16 +21,23 @@ const getData = async (page) => {
 }
 
 const CardList = async ({page}) => {
-    const data = await getData(page);
+
+    const {posts, count} = await getData(page);
+
+    const POST_PER_PAGE = 2;
+
+    const hasPrevious = POST_PER_PAGE * (page - 1) > 0;
+    const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < count;
+
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Recent Posts</h1>
             <div className={styles.posts}>
-                {data?.map((item) => (
-                    <Card item={item} key={item._id} />
+                {posts?.map((item) => (
+                    <Card item={item} key={item._id}/>
                 ))}
             </div>
-            <Pagination/>
+            <Pagination page={page} hasPrevious={hasPrevious} hasNext={hasNext}/>
         </div>
     )
 }
