288241192a50fdf013ddcf31917a699d22b6f978
AdrienHq <adrienhecq@gmail.com>
2024-03-21 18:15:46 +0100

    feat: count views per posts

--- src/app/api/posts/[slug]/route.js
+++ src/app/api/posts/[slug]/route.js
@@ -7,11 +7,11 @@ export const GET = async (req, {params}) => {
     const {slug} = params;
 
     try {
-        const post = await prisma.post.findUnique({
+        const post = await prisma.post.update({
             where: {slug},
-            include:{user: true}
+            data: {views: {increment: 1}},
+            include: {user: true}
         });
-
 
         return new NextResponse(
             JSON.stringify(post, {status: 200})
