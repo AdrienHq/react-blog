512a0009d04ffb7e9ccda7aa842e582422d1a519
AdrienHq <adrienhecq@gmail.com>
2024-03-22 17:45:03 +0100

    refactor: suppress inline

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,11 +5,9 @@ import Link from "next/link";
 
 const Card = ({key, item}) => {
     const date = new Date(item.createdAt);
-
     const day = date.getDate().toString().padStart(2, '0');
     const month = (date.getMonth() + 1).toString().padStart(2, '0');
     const year = date.getFullYear();
-
     const europeanDate = `${day}/${month}/${year}`;
 
     return (
